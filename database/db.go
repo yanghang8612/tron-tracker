892e0dd4767b6413ac8d4d6c44d831a0884d2dd8
Asuka <yanghang8612@163.com>
2023-12-01 19:41:32 +0800

    111

--- database/db.go
+++ database/db.go
@@ -16,7 +16,7 @@ type RawDB struct {
 }
 
 func New() *RawDB {
-	dsn := "root@tcp(127.0.0.1:3306)/tron?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:Root1234!@tcp(127.0.0.1:3306)/tron?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)

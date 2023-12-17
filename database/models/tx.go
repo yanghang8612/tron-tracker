6524c1a90029c7818f64193ad6c34571c0df9124
Asuka <yanghang8612@163.com>
2023-12-17 19:30:50 +0800

    111

--- database/models/tx.go
+++ database/models/tx.go
@@ -3,8 +3,8 @@ package models
 type Transaction struct {
 	ID                uint `gorm:"primaryKey"`
 	Hash              string
-	Owner             string `gorm:"char(21),index"`
-	To                string `gorm:"char(21),index"`
+	Owner             string `gorm:"size:21;index"`
+	To                string `gorm:"size:21;index"`
 	Height            uint
 	Timestamp         int64
 	Type              uint8  `gorm:"index:idx_key"`
@@ -22,10 +22,10 @@ type Transaction struct {
 
 type TRC20Transfer struct {
 	ID        uint   `gorm:"primaryKey"`
-	Hash      string `gorm:"char(32)"`
-	Token     string `gorm:"char(21),index"`
-	From      string `gorm:"char(21),index"`
-	To        string `gorm:"char(21),index"`
+	Hash      string `gorm:"size:32"`
+	Token     string `gorm:"size:21;index"`
+	From      string `gorm:"size:21;index"`
+	To        string `gorm:"size:21;index"`
 	Timestamp int64
 	Amount    BigInt
 }

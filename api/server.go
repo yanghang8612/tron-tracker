36413c930caf9b3a0cc16f60d3a073fa54a28471
Asuka <yanghang8612@163.com>
2024-01-08 12:59:00 +0800

    111

--- api/server.go
+++ api/server.go
@@ -127,14 +127,14 @@ func (s *Server) exchangesWeeklyStatistic(c *gin.Context) {
 	resultMap := make(map[string]*models.ExchangeStatistic)
 	for i := 0; i < 7; i++ {
 		for _, es := range s.db.GetExchangeStatistic(startDate.AddDate(0, 0, i).Format("060102")) {
-			exchangeName := regexp.MustCompile(`-hot|\s\d+$`).ReplaceAllString(es.Address, ``)
+			exchangeName := regexp.MustCompile(`-hot|-Hot|\s\d+$`).ReplaceAllString(es.Address, ``)
 			if _, ok := resultMap[exchangeName]; !ok {
 				es.Date = startDateStr + "~" + startDate.AddDate(0, 0, 7).Format("060102")
 				es.Name = exchangeName
 				es.Address = ""
 				resultMap[exchangeName] = &es
 			} else {
-				resultMap[es.Address].Merge(&es)
+				resultMap[exchangeName].Merge(&es)
 			}
 		}
 	}

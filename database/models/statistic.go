b2c8e8a826625d04a2d59be4ae13872acf96567c
Asuka <yanghang8612@163.com>
2024-05-06 15:11:46 +0800

    func(api): skip empty date for exchange statistic model

--- database/models/statistic.go
+++ database/models/statistic.go
@@ -272,7 +272,7 @@ func (o *TokenStatistic) Add(tx *Transaction) {
 
 type ExchangeStatistic struct {
 	ID                  uint   `gorm:"primaryKey" json:"-"`
-	Date                string `gorm:"index;size:6" json:"date"`
+	Date                string `gorm:"index;size:6" json:"date,omitempty"`
 	Name                string `json:"name,omitempty"`
 	Token               string `gorm:"index;" json:"token,omitempty"`
 	TotalFee            int64  `json:"total_fee"`

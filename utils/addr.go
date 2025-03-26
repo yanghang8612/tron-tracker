637b4ff90c18630dd01e5a832328deb5537f94d4
Asuka <yanghang8612@163.com>
2025-03-26 19:07:56 +0800

    func(utils): add exchange name trim rules

--- utils/addr.go
+++ utils/addr.go
@@ -16,7 +16,7 @@ func EncodeToBase58(addrInHex string) string {
 	return base58.CheckEncode(addressBytes, 0x41)
 }
 
-var trimRules = regexp.MustCompile(` Global| Reserves| Exchange| Deposit| hot wallet| Hot Wallet| Wallet|-hot|-Hot|-cold|-Cold| Cold|\s+\d+$`)
+var trimRules = regexp.MustCompile(` Global| Reserves| Exchange| Deposit| wallet| Wallet|-hot|-Hot| hot| Hot|-cold|-Cold| Cold| cold| warm| Warm|\s+\d+$`)
 
 func TrimExchangeName(name string) string {
 	if strings.Contains(name, "Bitpie") || strings.Contains(name, "bitpie") {

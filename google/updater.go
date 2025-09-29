2bcada4837c14ddfe0660a9fef384b404b1d9807
Asuka <yanghang8612@163.com>
2025-09-29 14:10:26 +0800

    func(updater): update total shares for TRON.Inc

--- google/updater.go
+++ google/updater.go
@@ -985,7 +985,7 @@ func (u *Updater) updateStockData(page *slides.Page, today time.Time) {
 	reqs = append(reqs, buildTextAndChangeRequests(volumeObjectId, -1, -1, avgDailyVolume, avgDailyVolumeChange, 11, 7, true)...)
 
 	// Update the market cap
-	marketCap := "$" + common.FormatWithUnits(todayData[4].(float64)*3346.8e4)
+	marketCap := "$" + common.FormatWithUnits(todayData[4].(float64)*25346.8e4)
 	marketCapChange := common.FormatFloatChangePercent(oneWeekAgoData[4].(float64), todayData[4].(float64))
 	marketCapObjectId := page.PageElements[15].ObjectId
 	reqs = append(reqs, buildTextAndChangeRequests(marketCapObjectId, -1, -1, marketCap, marketCapChange, 11, 7, true)...)
@@ -1065,6 +1065,8 @@ func (u *Updater) updateStockData(page *slides.Page, today time.Time) {
 		"[Low/High]分别为上周内的最低价与最高价\n"+
 		"[Daily Avg Vol]为股票过去五个交易日内日均交易量（按美元计价）\n"+
 		"[Market Cap]为股票以昨日的收盘价计算的总市值\n"+
+		"  - 本PPT中Cap以253.47M的总股本计算，其中包含普通流通股33.47M股+ PIPE Warrants（220M 股）\n"+
+		"  - 注意不同网站在计算Cap时在计不计入PIPE Warrants上有差异，导致其显示市值不同\n"+
 		"[Value of digital assets held]为SRM的关联TRON地址持有的代币的总价值\n"+
 		"链上地址目前只持有一种代币为sTRX，共持有%s枚sTRX\n\n"+
 		"sunewikeSRM: TFZZx3HXBEGqA1hJnYmRvscjS48gihWXY6\n"+

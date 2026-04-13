859fe90ab1a93ffac19255e1df4e77b3f1b8c9d1
Asuka <yanghang8612@163.com>
2026-04-13 15:13:59 +0800

    func(model): rename some json fields for exchange model

--- database/models/exchange.go
+++ database/models/exchange.go
@@ -3,9 +3,9 @@ package models
 type Exchange struct {
 	ID         uint   `gorm:"primaryKey"  json:"-"`
 	Address    string `gorm:"size:34;index" json:"address"`
-	Name       string
-	OriginName string `json:"name"`
-	FromAsuka  bool   `json:"-"`
+	Name       string `json:"tag"`
+	OriginName string `json:"origin_name"`
+	FromAsuka  bool   `json:"from_asuka"`
 }
 
 type Exchanges struct {
